5485ec027317876801feafe227ecc25fed0e2b65
clei <clei@s10.etu.info-ufr.univ-montp2.fr>
2013-09-19 09:59:39 +0200

    modifier the sex

--- HelloWorld/main.java
+++ HelloWorld/main.java
@@ -1,7 +1,7 @@
 public class main{
 	public static void main(String[] args){
 		HelloWorld hello1 = new HelloWorld("cheng");
-		HelloWorldGender hello2 = new HelloWorldGender("cheng",true);
+		HelloWorldGender hello2 = new HelloWorldGender("cheng",false);
 
 
 		hello1.print();	
